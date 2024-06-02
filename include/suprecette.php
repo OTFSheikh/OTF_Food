f8481aca4cbc4b6bc5820d9d28bea2742283c51f
OTFSheikh <155764817+OTFSheikh@users.noreply.github.com>
2024-06-02 15:55:58 +0000

    Supprimer recette

---
+++ include/suprecette.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+if ($_GET && isset($_SESSION['id'])) {
+    $id=$_GET['id'];
+    require_once 'start_bdd.php';
+
+    $req = $bdd->prepare("DELETE FROM recette where idrecette=:id");
+    $req->bindvalue(":id", $id);
+    $req->execute();
+    header("location:../recette.php");
+}
